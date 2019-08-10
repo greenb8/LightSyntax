b68ced6194b48b538c86f3fb8cbcb9e7798f61a9
NickProgramm <47205258+NickProgramm@users.noreply.github.com>
2019-08-10 12:12:54 +0700

    Add Header file

---
+++ LightSyntax.h
@@ -0,0 +1,12 @@
+/*
+* LightSyntax
+* (C) 2019 NickProgramm
+* https://github.com/NickProgramm/LightSyntax
+*/
+#include "Arduino.h"
+void input(int Pin);
+void output(int Pin);
+void Dhigh(int Pin);
+void Ahigh(int Pin);
+void Alow(int Pin);
+void Dlow(int Pin);
