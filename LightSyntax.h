da7aa1953b6bb32675c0e1ae2d68832889649b02
NickProgramm <47205258+NickProgramm@users.noreply.github.com>
2019-08-29 23:11:24 +0700

    Update LightSyntax.h

--- LightSyntax.h
+++ LightSyntax.h
@@ -3,6 +3,10 @@
 * (C) 2019 NickProgramm
 * https://nickprogramm.github.io/LightSyntax/
 */
+
+#ifndef lightsyntax_h
+#define lightsyntax_h
+
 #include "Arduino.h"
 void input(int Pin);
 void output(int Pin);
@@ -10,3 +14,4 @@ void Dhigh(int Pin);
 void Ahigh(int Pin);
 void Alow(int Pin);
 void Dlow(int Pin);
+#endif
